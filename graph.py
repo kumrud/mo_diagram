66e24cff13954216d7914147617e4ec422b7ea06
Taewon D. Kim <kimt33@mcmaster.ca>
2016-06-05 12:07:08 -0400

    Add adjacency matrix
    What:
     1. Change function name
     2. Add adjacency matrix to the graph
     3. Little clean up

--- graph.py
+++ graph.py
@@ -1,43 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools as it
 
 __author__ = 'Kumru'
 
 
-def generate_graph(num_point, theta=None):
-    """ Generates correlation graph as circle
+def generate_circle_graph(num_point, adjacency=None):
+    """ Generates complete graph arranged in a circle
 
     Parameters
     ----------
-    theta : np.array
-        The angular values
+    num_points : int
+        Number of vertices on the graph
+    adjacency : np.ndarray(K, K)
+        Adjacency matrix of the graph
     """
 
     # Set points in circle
-    if theta is None:
-        theta = np.linspace(0, 2 * np.pi, num_point+1)
+    theta = np.linspace(0, 2 * np.pi, num_point+1)
     x_point = np.cos(theta)
     y_point = np.sin(theta)
     point = [list(i) for i in zip(x_point, y_point)]
 
-    xs = [i[0] for i in point]
-    ys = [i[1] for i in point]
-
     # Plot points
-    fig, ax = plt.subplots()
-    ax = fig.add_subplot(111)
-    ax.scatter(xs, ys, color='red')
+    fig = plt.figure()
+    plt.scatter(x_point, y_point, color='black')
 
+    if adjacency is None:
+        adjacency = np.ones((num_point, num_point), dtype=bool)
+        adjacency -= np.diag(np.diag(adjacency))
+    else:
+        assert adjacency.shape[0] == adjacency.shape[1] == num_point, ''
+        'Given adjacency matrix does not match the number of points'
+
+    lines = []
     # Plot lines
-    for i in point:
-        for j in point:
-            if i != j:
-                print i, j
-                x_line = [i[0], j[0]]
-                y_line = [i[1], j[1]]
-                line, = ax.plot(x_line, y_line, color='green')
-            else:
-                pass
+    for indices in zip(*np.nonzero(np.triu(adjacency))):
+        x_line = [x_point[i] for i in indices]
+        y_line = [y_point[i] for i in indices]
+        lines.append(plt.plot(x_line, y_line, color='blue', linestyle='dashed', solid_capstyle='round', linewidth=2.5)[0])
 
     # Set line visibility by clicking
     def onpick(event):
@@ -49,3 +50,9 @@ def generate_graph(num_point, theta=None):
     fig.canvas.mpl_connect('pick_event', onpick)
     plt.show()
 
+
+adjacency = np.array([[0, 1, 1, 0],
+                         [1, 0, 0, 1],
+                         [1, 0, 0, 1],
+                         [0, 1, 1, 0]])
+generate_circle_graph(4, adjacency)
