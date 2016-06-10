541a7b693c249ecb13555ee3096db88e8172c9b0
Taewon D. Kim <kimt33@mcmaster.ca>
2016-06-09 22:05:07 -0400

    Rename num_point to nump_points

--- graph.py
+++ graph.py
@@ -5,37 +5,32 @@ import itertools as it
 
 __author__ = 'Kumru'
 
-on_alpha = 1.0
-off_alpha = 0.1
-
-def generate_circle_graph(num_point, adjacency=None):
+def generate_circle_graph(fig, ax, num_points, adjacency=None, on_alpha=1.0, off_alpha=0.1):
     """ Generates complete graph arranged in a circle
 
     Parameters
     ----------
-    num_points : int
+    num_pointss : int
         Number of vertices on the graph
     adjacency : np.ndarray(K, K)
         Adjacency matrix of the graph
     """
 
     # Set points in circle
-    theta = np.linspace(0, 2 * np.pi, num_point+1)
+    theta = np.linspace(0, 2 * np.pi, num_points+1)
     x_point = np.cos(theta)
     y_point = np.sin(theta)
     point = [list(i) for i in zip(x_point, y_point)]
 
     # Plot points
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
     ax.scatter(x_point, y_point, color='black')
     # ax.plot(x_point, y_point, color='black', picker=True)
 
     if adjacency is None:
-        adjacency = np.ones((num_point, num_point), dtype=bool)
+        adjacency = np.ones((num_points, num_points), dtype=bool)
         adjacency -= np.diag(np.diag(adjacency))
     else:
-        assert adjacency.shape[0] == adjacency.shape[1] == num_point, ''
+        assert adjacency.shape[0] == adjacency.shape[1] == num_points, ''
         'Given adjacency matrix does not match the number of points'
 
     # Plot lines
@@ -48,18 +43,20 @@ def generate_circle_graph(num_point, adjacency=None):
     # Set line visibility by clicking
     def onpick(event):
         thisline = event.artist
+        print thisline
         if thisline.get_alpha() == on_alpha:
             thisline.set_alpha(off_alpha)
         elif thisline.get_alpha() == off_alpha:
             thisline.set_alpha(on_alpha)
         fig.canvas.draw()
     fig.canvas.mpl_connect('pick_event', onpick)
-    plt.show()
     return fig
-
 
 # adjacency = np.array([[0, 1, 1, 0],
 #                          [1, 0, 0, 1],
 #                          [1, 0, 0, 1],
 #                          [0, 1, 1, 0]])
-generate_circle_graph(10, )
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+# generate_circle_graph(fig, ax, 10, )
+# plt.show()
