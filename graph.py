1e65711946f91c5d8b53b4cb4a21243914f9a327
Taewon D. Kim <kimt33@mcmaster.ca>
2016-06-12 19:38:34 -0400

    Make graph return plot object

--- graph.py
+++ graph.py
@@ -18,13 +18,13 @@ def generate_circle_graph(fig, ax, num_points, adjacency=None, on_alpha=1.0, off
 
     # Set points in circle
     theta = np.linspace(0, 2 * np.pi, num_points+1)
-    x_point = np.cos(theta)
-    y_point = np.sin(theta)
+    x_point = np.cos(theta)[:-1]
+    y_point = np.sin(theta)[:-1]
     point = [list(i) for i in zip(x_point, y_point)]
 
     # Plot points
-    ax.scatter(x_point, y_point, color='black')
-    # ax.plot(x_point, y_point, color='black', picker=True)
+    scat = ax.scatter(x_point, y_point, color='black')
+    # ax.plot(x_point, y_point, color='black', picker=True, linestyle='None')
 
     if adjacency is None:
         adjacency = np.ones((num_points, num_points), dtype=bool)
@@ -35,7 +35,7 @@ def generate_circle_graph(fig, ax, num_points, adjacency=None, on_alpha=1.0, off
 
     # Plot lines
     lines = []
-    for indices in zip(*np.nonzero(np.triu(adjacency))):
+    for indices in zip(*np.where(np.triu(adjacency))):
         x_line = [x_point[i] for i in indices]
         y_line = [y_point[i] for i in indices]
         lines.append(ax.plot(x_line, y_line, color='blue', alpha=on_alpha, linestyle='dashed', solid_capstyle='round', linewidth=2.5, picker=10)[0])
@@ -43,14 +43,12 @@ def generate_circle_graph(fig, ax, num_points, adjacency=None, on_alpha=1.0, off
     # Set line visibility by clicking
     def onpick(event):
         thisline = event.artist
-        print thisline
-        if thisline.get_alpha() == on_alpha:
-            thisline.set_alpha(off_alpha)
-        elif thisline.get_alpha() == off_alpha:
-            thisline.set_alpha(on_alpha)
+        old_alpha = thisline.get_alpha()
+        new_alpha = on_alpha+off_alpha-old_alpha
+        thisline.set_alpha(new_alpha)
         fig.canvas.draw()
     fig.canvas.mpl_connect('pick_event', onpick)
-    return fig
+    return scat
 
 # adjacency = np.array([[0, 1, 1, 0],
 #                          [1, 0, 0, 1],
@@ -58,5 +56,5 @@ def generate_circle_graph(fig, ax, num_points, adjacency=None, on_alpha=1.0, off
 #                          [0, 1, 1, 0]])
 # fig = plt.figure()
 # ax = fig.add_subplot(1, 1, 1)
-# generate_circle_graph(fig, ax, 10, )
+# scat = generate_circle_graph(fig, ax, 10, )
 # plt.show()
