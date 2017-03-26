b5070be8c7619ebc12fee41100360838542ea176
Kumru Dikmenli <kumru.dikmenli@gmail.com>
2017-03-26 01:43:09 -0400

    Add line_data and moved degenerate
    Added line_data for getting x, y and color for the graph
    Moved degenerate out of the MoDiagram class

--- diagram.py
+++ diagram.py
@@ -44,11 +44,11 @@ class MoDiagram(object):
         assert (ab_energies.shape[0] <= mo_energies.shape[0]),\
             'Ao number is greater than Mo'
         assert (isinstance(coeff_ab_mo, np.ndarray) and
-                len(coeff_ab_mo.shape) == 2),\
+            len(coeff_ab_mo.shape) == 2), \
             'Coefficient matrix given is not a two dimentional array'
         assert (isinstance(occupations, np.ndarray) and
-                len(occupations.shape) == 1,\
-            'Occupations is not given as a one dimensional numpy array',\
+                len(occupations.shape)) == 1,\
+            'Occupations is not given as a one dimensional numpy array'
         assert (len(occupations) == len(mo_energies)),\
             'Number of occupations is not equal to number of molecular orbitals'
         assert occupations.dtype in [float],\
@@ -59,93 +59,61 @@ class MoDiagram(object):
         self._ab_energies = ab_energies
         self._coeff_ab_mo = coeff_ab_mo
         self._occupations = occupations
-        self._hard_way = False
 
-        @property
-        def mo_energies(self):
-            '''Molecular/QAO energies
-            '''
-            if not self._hard_way:
-                return self._mo_energies
+    @property
+    def mo(self):
+        '''Molecular/QAO energies
+        '''
+        return self._mo_energies
 
-        @property
-        def ab_energies(self):
-            '''Atomic orbital energies
-            '''
-            if not self._hard_way:
-                return self._ab_energies
+    @property
+    def ao(self):
+        '''Atomic orbital energies
+        '''
+        return self._ab_energies
 
-        @property
-        def coeff_ab_mo(self):
-            '''Atomic orbital energies
-            '''
-            if not self._hard_way:
-                return self._coeff_ab_mo
+    @property
+    def coeff(self):
+        '''Coefficient matrix from atomic basis to molecular
+        '''
+        return self._coeff_ab_mo
 
-        @property
-        def occupations(self):
-            '''Atomic orbital energies
-            '''
-            return self._occupations
+    @property
+    def occupations(self):
+        '''Occupation of molecular orbitals
+        '''
+        return self._occupations
 
-        @property
-        def degen_ao(self):
-            '''Degeneracy atomic orbital
-            '''
-            degen_ao = degen(ab_energies)
-            return degen_ao
+    @property
+    def degen_ao(self):
+        '''Degeneracy atomic orbital
+        '''
+        degen_ao = degenerate(self._ab_energies)
+        return degen_ao
 
-        @property
-        def degen_mo(self):
-            '''Degeneracy atomic orbital
-            '''
-            degen_mo = degen(self._mo_energies)
-            return degen_mo
+    @property
+    def degen_mo(self):
+        '''Degeneracy atomic orbital
+        '''
+        degen_mo = degenerate(self._mo_energies)
+        return degen_mo
 
-        @property
-        def num_ao:
-            ''' Number of atomic orbitals
-            '''
-            return self._ab_energies[0]
+    @property
+    def num_ao(self):
+        ''' Number of atomic orbitals
+        '''
+        return self._ab_energies[0]
 
-        @property
-        def num_mo:
-            ''' Number of atomic orbitals
-            '''
-            return self._mo_energies[0]
+    @property
+    def num_mo(self):
+        ''' Number of atomic orbitals
+        '''
+        return self._mo_energies[0]
 
-        def degenerate(self, energies, tol=0.01):
-            '''
-            Sets degenerate energy levels using histogram
-
-            Parameters
-            ----------
-            energies: np.ndarray
-                Molecular/Atomic orbital energies
-            tol: float
-                Tolerance for degeneracy between two energies
-
-            Returns
-            -------
-            degen: np.ndarray
-                Degeneracy of orbitals
-
-            Example
-            -------
-            energies = np.array([2, 2, 5, 6, 7, 7, 8])
-            degens = array([2, 1, 1, 2, 1])
-
-            '''
-
-            energy_diff = np.diff(energies)
-            # a new line after these indices
-            degen_indices = [i for i,j in enumerate(energy_diff) if j>tol]
-            # histogram range right non-inclusive 
-            energy_bins = [energies[0]]+[energies[i+1] for i in degen_indices]+[energies[-1]+1]
-            degen = np.histogram(energies, bins=energy_bins)[0]
-
-            return degen
-
+    # def __getattr__(self, attr):
+    #     ''' Return class attribute
+    #     '''
+    #     return self._dict__[attr]
 
 
 class OrbitalPlot(object):
@@ -168,45 +136,149 @@ class OrbitalPlot(object):
         Coordinated for lines
     make_line
         Creates lines as matplotlib.collections.BrokenBarHCollection instance
+        TODO: specify graph type (ao, mo or quambo)
+            Selecting orbitals to graph
     
-    TODO:
-    line_connect
-        How to connect two lines on different graphs
+    TODO:line_connect, How to connect two lines on different graphs
+
 
     '''
-    def __init__(self, x_length=1.0, x_sep=0.1, y_length=0.01):
+    def __init__(self, data, x_length=1.0, x_sep=0.1, y_length=0.1):
         self._x_length = x_length
         self._x_sep = x_sep
         self._y_length = y_length
 
+        if not isinstance(data, MoDiagram):
+            raise TypeError('Given data has to be an MoDiagram instance')
+        self._data = data
+
     @property
     def x_length(self):
+        ''' Length of a line
+        '''
         return self._x_length
 
     @property
     def x_sep(self):
+        ''' Separation between each degenerate level
+        '''
         return self._x_sep
 
-    def line_data(self, degens, energies):
-        x_start = -(degens//2)*(self.x_length+self.x_sep) - (degens%2)*self.x_length/2.0
+    @property
+    def x_shift(self):
+        ''' Length of which a line will be shifted in case of degeneracy
+        '''
+        return self._x_length + self._x_sep
 
+    @property
+    def y_length(self):
+        ''' Thickness of the line
+        '''
+        return self._y_length
+
+    @property
+    def occupations(self):
+        return self._data.occupations
+
+    def line_data(self, energies):
+        '''
+        Sets x and y coordinates for lines
+
+        Parameters
+        ----------
+        energies: np.ndarray
+            Molecular/Atomic orbital energies
+
+        Returns
+        -------
+        x_coor, y_coor: list of tuples
+            Start and length of x and y coordinates
+        line_color: tuple
+            Color lines according to occupation
+
+        Example
+        -------
+        energies = [0 1 1 2 3 3 3]
+        x_coor = [(0.55, 1), (0.0, 1), (1.1, 1), (0.55, 1), (-0.44999999999999996, 1),
+            (0.6500000000000001, 1), (1.7500000000000002, 1)]
+        y_coor = [(0, 0.01), (1, 0.01), (1, 0.01), (2, 0.01), (3, 0.01), (3, 0.01), (3, 0.01)]
+
+        '''
+        degens = degenerate(energies)
+
+        # leftmost x coordinates for a line
+        x_start = [(d & 2) * self.x_shift / 2 - self.x_shift * (d - 1) // 2 for d in degens]
+
+        # unpack histogram degeracy to match energy levels
         x_data = []
         for i,j in zip(degens, x_start):
-            if i != 1: x_data += [j + k * (self.x_length + self.x_sep) for k in range(int(i))]
+            if i != 1: x_data += [j + k * (self.x_shift) for k in range(int(i))]
             if i == 1: x_data.append(j)
 
+        # zip start and length of x and y coordinates
         x_coor = [(i, self.x_length) for i in x_data]
         y_coor = [(i, self.y_length) for i in energies]
 
-        return x_coor, y_coor
+        # line colors as occupation
+        line_color = []
+        for i in self.occupations:
+            if i == 2: line_color.append("green")
+            elif i == 1: line_color.append("yellow")
+            elif i == 0: line_color.append("red")
+            # Should not happen
+            else: line_color.append("blue")
+        tuple(line_color)
 
-    def make_line(self, energies):
-        degens = degenerate(energies)
-        x_coor, y_coor = line_data(degens, energies)
+        return x_coor, y_coor, line_color
 
+    def make_line(self, *args):
+        '''TODO: Check arguments
+            shift multiple plots
+        '''
         fig, ax = plt.subplots()
-        line = [ax.broken_barh([x_coor[i]], y_coor[i]) for i,j in enumerate(energies)]
-        return line
+        ax.set_xlim(-4,4)
+        ax.set_ylim(-21,2)
+        for energy in args:
+            x, y, color = self.line_data(energy)
+
+            line = [ax.broken_barh([x[i]], y[i], facecolor=color[i]) for i,j in enumerate(energy)]
+            return line
+
+    def make_graph(self, *args):
+        pass
 
 
-    
+def degenerate(energies, tol=0.01):
+    '''
+    Sets degenerate energy levels using histogram
+
+    Parameters
+    ----------
+    energies: np.ndarray
+        Molecular/Atomic orbital energies
+    tol: float
+        Tolerance for degeneracy between two energies
+
+    Returns
+    -------
+    degens: np.ndarray
+        Degeneracy of orbitals
+
+    Example
+    -------
+    energies = np.array([2, 2, 5, 6, 7, 7, 8])
+    degens = array([2, 1, 1, 2, 1])
+
+    '''
+
+    energy_diff = np.diff(energies)
+    # a new line after these indices
+    degen_indices = [i for i,j in enumerate(energy_diff) if j>tol]
+    print degen_indices
+    # histogram range right non-inclusive
+    energy_bins = [energies[0]]+[energies[i+1] for i in degen_indices]+[energies[-1]+1]
+    print energy_bins
+    print energies
+    degens = np.histogram(energies, bins=energy_bins)[0]
+    return degens
+
